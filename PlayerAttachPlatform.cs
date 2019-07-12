5aaf724aa8aec6f738b1dc86e55a3e9075b170ef
gloomyBreeder <1riff-raff1@mail.ru>
2019-07-12 16:02:20 +0300

    Update PlayerAttachPlatform.cs
    Added a comment

--- PlayerAttachPlatform.cs
+++ PlayerAttachPlatform.cs
@@ -1,24 +1,24 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>Easy way for moving platforms if your character has Character Controller. This script keeps your character on platform by parenting it.
+/// Put it on your platform which should also have 2 colliders: basic one and one with IsTrigger checked </summary>
 public class PlayerAttachPlatform : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        if (other.GetComponent<MainCharacterController>() != null)
+        if (other.GetComponent<CharacterController>() != null)
         {
-            other.GetComponent<MainCharacterController>().YouAreOnPlatform(true, transform);
             other.transform.SetParent(transform, true);
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.GetComponent<MainCharacterController>() != null)
+        if (other.GetComponent<CharacterController>() != null)
         {
             other.transform.SetParent(null, true);
-            other.GetComponent<MainCharacterController>().YouAreOnPlatform(false);
         }
     }
 }
