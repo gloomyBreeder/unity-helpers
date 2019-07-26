3bf9692305346d451d27a16553cb529b84a0a98a
Val <1riff-raff1@mail.ru>
2019-07-26 16:28:27 +0400

    Add files via upload
    Super easy script for debugging path of NavMesh agent

---
+++ NavMeshPathDebugger.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavMeshPathDebugger : MonoBehaviour
+{
+  /// <summary> It's a super easy script can help you to debug path of your NavMesh agent
+  /// For example, you have a player and AI who follows your player. If you attach
+  /// LineRenderer component and this script to player you'll see calculated AI path from AI to player </summary>
+  [SerializeField]
+  private NavMeshAgent _agent;
+  private LineRenderer _linerenderer;
+
+  void Start()
+  {
+    _linerenderer = GetComponent<LineRenderer>();
+  }
+
+  void Update()
+  {
+    if (_agent.hasPath)
+    {
+      _linerenderer.positionCount = _agent.path.corners.Length;
+      _linerenderer.SetPositions(_agent.path.corners);
+      _linerenderer.enabled = true;
+    }
+    else
+    {
+      _linerenderer.enabled = false;
+    }
+
+  }
+}
